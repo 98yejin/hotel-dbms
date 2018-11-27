f8c0a988e8e1799ce9c4dccfff039f15c5c9ce0f
김지은 <kimjieun@gimjieun-ui-MacBook-Pro.local>
2018-11-27 13:50:04 +0900

    sql add

---
+++ department.sql
@@ -0,0 +1,4 @@
+INSERT INTO department VALUES (1, 'manager', 'manage', '101', 00000);
+INSERT INTO department VALUES (2, 'front_desk', 'guide&check', '102', 00001);
+INSERT INTO department VALUES (3, 'room_charge', 'room_manage', '103', 00002);
+INSERT INTO department VALUES (4, 'cleaning', 'room_cleaning', '104', 00003);
---
+++ valuable.sql
@@ -0,0 +1,2 @@
+INSERT INTO valuable (content, custom_id) VALUES ('red carrier', 1);
+INSERT INTO valuable (content, custom_id) VALUES ('black backpack', 1);
